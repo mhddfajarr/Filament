ad34ad3de5cf080850ceef3bb68ada13edba9716
Muhammad Fajar <mhddfajar@gmail.com>
2024-05-16 10:26:50 +0700

    change navigation icon

--- app/Filament/App/Resources/DepartmentResource.php
+++ app/Filament/App/Resources/DepartmentResource.php
@@ -23,7 +23,7 @@ class DepartmentResource extends Resource
 {
     protected static ?string $model = Department::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-academic-cap';
 
     public static function form(Form $form): Form
     {
